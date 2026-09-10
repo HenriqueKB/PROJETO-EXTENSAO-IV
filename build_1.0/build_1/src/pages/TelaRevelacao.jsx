0df3bb6e4b37c7dd2fe77c892bd599576d4a1116
HenriqueKB <108744339+HenriqueKB@users.noreply.github.com>
2026-09-10 16:45:48 -0300

    Lógica de avançar etapa E revelar os papéis dos jogadores feita

--- build_1.0/build_1/src/pages/TelaRevelacao.jsx
+++ build_1.0/build_1/src/pages/TelaRevelacao.jsx
@@ -5,12 +5,31 @@ const TelaRevelacao = ({ jogadores, impostor, palavraSecreta, onProximaEtapa }) 
   const [indexAtual, setIndexAtual] = useState(0)
   const [revelado, setRevelado] = useState(false)
 
+
+  // 2. Funções pra checar o estado dos jogadores
+
   // Descobre qual o nome do jogador da vez baseado no índice
-  const jogadorDaVez = jogadores[indexAtual]
+    const jogadorDaVez = jogadores[indexAtual]
 
   // Verifica se o jogador da vez é o impostor
-  const ehOImpostor = jogadorDaVez === impostor
+    const ehOImpostor = jogadorDaVez === impostor
 
-    setRevelado(false)
-  // ... lógica dos botões de avançar e revelar
-}
+ // Verifica se já está no último jogador da vez
+    const ehOUltimoJogador = indexAtual === jogadores.length - 1
+
+  // Lógica dos botões de avançar e revelar 
+    const handleRevelar = () => {
+        setRevelado(true)
+    }
+
+    const handleProximo = () => {
+        setRevelado(false)
+
+        if (indexAtual === jogadores.length - 1) {
+           onProximaEtapa() // Avanca o jogo
+
+        } else {
+            setIndexAtual(indexAtual + 1) // Passa a posição pro próximo
+        }
+        }
+};
