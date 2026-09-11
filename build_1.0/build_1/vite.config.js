766301e975c9abb0aff244db72be80a0b80f8ec7
HenriqueKB <108744339+HenriqueKB@users.noreply.github.com>
2026-09-11 15:29:10 -0300

    configurando sistema de dfeploy e subindo como um site!

--- build_1.0/build_1/vite.config.js
+++ build_1.0/build_1/vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+base: '/PROJETO-EXTENSAO-IV/', // 👈 Subsitua "nome-do-seu-repositorio" pelo nome do seu repo no GitHub!
 })
