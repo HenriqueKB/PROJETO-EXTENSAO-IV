363b91ae257581d304a9d8a582251ecb7dd35e8a
HenriqueKB <108744339+HenriqueKB@users.noreply.github.com>
2026-09-17 15:13:49 -0300

    Arrumei as regras

--- build_1.0/src/pages/TelaRegras.jsx
+++ build_1.0/src/pages/TelaRegras.jsx
@@ -8,8 +8,8 @@ const TelaRegras = ({ onIrParaCadastro }) => {
           <li><strong>Objetivo:</strong> Os inocentes devem descobrir o Impostor, enquanto ele tenta se camuflar.</li>
           <li><strong>Funções:</strong> Cada participante verá sua função em segredo no modo <em>hot-seat</em>.</li>
           <li><strong>Dicas:</strong> Os inocentes recebem a palavra secreta; o Impostor recebe apenas uma dica genérica.</li>
-          <li><strong>Discussão:</strong> O grupo debate por 2 minutos para fazer perguntas e pegar blefes.</li>
-          <li><strong>Votação:</strong> O grupo escolhe o acusado final para revelar o vencedor.</li>
+          <li><strong>Discussão:</strong> O grupo debate por 2 minutos, onde cada um deve dar uma dica sobre a palavra sorteada, enquanto o impostor tenta acompanhar sem se revelar.</li>
+          <li><strong>Votação:</strong> O grupo escolhe o acusado final para então encerrar o jogo e descobrir quem era o impostor.</li>
         </ul>
       </div>
 
